0dad3db11302a5b229c97160a5ad181302f9ed2a
ZeeQ <kw133875@gmail.com>
2025-11-16 23:35:29 -0600

    fixed a layout issue for board update form

--- client/src/features/board/forms/BoardUpdateForm.jsx
+++ client/src/features/board/forms/BoardUpdateForm.jsx
@@ -35,60 +35,82 @@ export default function BoardUpdateForm({
     <>
       <ModalOverlay onClose={() => setShowEditBoardModal(false)}>
         {error && <p>{error}</p>}
-        <form className="boardUpdateForm" onSubmit={handleUpdateBoard}>
-          <label>
-            <input
-              className="boardName"
-              type="text"
-              placeholder={board.name}
-              name="name"
-            />
-          </label>
-          <label>
-            <input
-              className="boardDescription"
-              type="text"
-              placeholder={board.description}
-              name="description"
-            />
-          </label>
-          <button className="updateBoard" type="submit">
-            Update Board
-          </button>
-        </form>
-        <button
-          className="showCreateTaskModal"
-          onClick={() => setShowCreateTaskModal(true)}
-        >
-          Create Task
-        </button>
+        <div className="boardUpdateContainer flex gap-9">
+          <div className="boardUpdateFormContent">
+            <h2 className="boardUpdateTitle text-2xl font-bold mb-8">
+              Update Board
+            </h2>
+            <form
+              className="boardUpdateForm flex flex-col gap-4 border-r pr-9 border-gray-300"
+              onSubmit={handleUpdateBoard}
+            >
+              <label>
+                <input
+                  className="boardName"
+                  type="text"
+                  placeholder={board.name}
+                  name="name"
+                />
+              </label>
+              <label>
+                <input
+                  className="boardDescription"
+                  type="text"
+                  placeholder={board.description}
+                  name="description"
+                />
+              </label>
+              <button className="updateBoard" type="submit">
+                Update Board
+              </button>
+            </form>
+          </div>
+          <div className="boardOptions">
+            <h2 className="boardOptionsTitle text-2xl font-bold mb-8">
+              Board Options
+            </h2>
+            <div className="boardOptionsButtons flex flex-col gap-4">
+              <button
+                className="showCreateTaskModal"
+                onClick={() => setShowCreateTaskModal(true)}
+              >
+                Create Task
+              </button>
 
-        <button className="deleteBoard" onClick={() => deleteBoard(board.id)}>
-          Delete Board
-        </button>
-        <button
-          className="showDeleteAllTasksModal"
-          onClick={() => setShowDeleteAllTasksModal(true)}
-        >
-          Delete All Tasks
-        </button>
+              <button
+                className="deleteBoard"
+                onClick={() => deleteBoard(board.id)}
+              >
+                Delete Board
+              </button>
+              <button
+                className="showDeleteAllTasksModal"
+                onClick={() => setShowDeleteAllTasksModal(true)}
+              >
+                Delete All Tasks
+              </button>
+            </div>
+          </div>
+        </div>
         {showDeleteAllTasksModal && (
           <ModalOverlay onClose={() => setShowDeleteAllTasksModal(false)}>
-            <p className="confirmationMessage">
-              Are you sure you want to delete all tasks?
-            </p>
-            <button
-              className="deleteAllTasks"
-              onClick={() => deleteAllTasks(board.id)}
-            >
-              Yes
-            </button>
-            <button
-              className="cancelDeleteAllTasks"
-              onClick={() => setShowDeleteAllTasksModal(false)}
-            >
-              No
-            </button>
+            <div className="deleteAllTasksModalContent flex flex-col gap-4 justify-center items-center">
+              <p className="confirmationMessage">
+                Are you sure you want to delete all tasks?
+              </p>
+              <button
+                className="deleteAllTasks"
+                onClick={() => deleteAllTasks(board.id)}
+              >
+                Yes
+              </button>
+              <button
+                className="cancelDeleteAllTasks"
+                onClick={() => setShowDeleteAllTasksModal(false)}
+              >
+                No
+              </button>
+            </div>
           </ModalOverlay>
         )}
       </ModalOverlay>
