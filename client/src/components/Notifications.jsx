ca7f759c86b4abe48a6a368d90892c643f179273
ZeeQ <kw133875@gmail.com>
2025-11-30 19:40:57 -0600

    fixed up more styling, styled notification banner

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -5,9 +5,14 @@ export default function Notifications() {
   const { notification, setNotification } = useBoards();
   if (!notification) return null;
   return (
-    <div className="notification-banner fixed top-0 right-0">
-      {notification}
-      <button onClick={() => setNotification(null)}>Dismiss</button>
+    <div className="notification-banner flex items-center gap-4 fixed top-0 right-0 z-50 bg-white shadow-md rounded-lg p-4 border border-gray-200">
+      <p className="text-gray-500">{notification}</p>
+      <button
+        className="bg-black text-white px-2 py-1 rounded-md hover:bg-gray-800 transition-colors duration-200 cursor-pointer"
+        onClick={() => setNotification(null)}
+      >
+        Dismiss
+      </button>
     </div>
   );
 }
--- client/src/features/task/TaskList.jsx
+++ client/src/features/task/TaskList.jsx
@@ -6,7 +6,7 @@ export default function TaskList({ tasks, boardId }) {
   return (
     <ul className="taskList flex gap-4 lg:justify-center grow">
       <div className="taskListStatusGrid md:bg-gray-100 gap-2 grid grid-cols-1 md:grid-cols-2 lg:flex gap-10 grow">
-        <div className="taskListCompletedList flex flex-col gap-4 grow basis-full p-4 rounded-md bg-green-100/50 border border-green-200 lg:justify-center lg:items-center">
+        <div className="taskListCompletedList flex flex-col gap-4 grow basis-full p-4 rounded-md bg-green-100/50 border border-green-200 lg:items-center">
           <div className="taskListCompletedIcon flex items-center gap-2">
             <span className="bg-green-500 w-4 h-4 rounded-full"></span>
             <p className="text-gray-500">Completed</p>
@@ -23,7 +23,7 @@ export default function TaskList({ tasks, boardId }) {
           </div>
         </div>
 
-        <div className="taskListNotCompletedList flex flex-col gap-4 grow basis-full p-4 rounded-md bg-red-100/50 border border-red-200 lg:justify-center lg:items-center">
+        <div className="taskListNotCompletedList flex flex-col gap-4 grow basis-full p-4 rounded-md bg-red-100/50 border border-red-200 lg:items-center">
           <div className="taskListNotCompletedIcon flex items-center gap-2">
             <span className="bg-red-500 w-4 h-4 rounded-full"></span>
             <p className="text-gray-500">Not Completed</p>
